3ac4570a89b0d19bb622c322d09ed788ce227ce6
Maria Pineda <marie.izq@gmail.com>
2024-07-30 15:35:21 -0600

    se añadio borrar y nuevo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+//import { useState, useEffect } from "react";
 import "primereact/resources/themes/saga-blue/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
@@ -6,21 +6,163 @@ import "primeflex/primeflex.css";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 import { heroService } from "./service/heroService";
-import { InputText } from 'primereact/inputtext';
+import { InputText } from "primereact/inputtext";
+
+import React, { useState, useEffect, useRef } from "react";
+import { Button } from "primereact/button";
+import { Toast } from "primereact/toast";
+import { IconField } from "primereact/iconfield";
+import { InputIcon } from "primereact/inputicon";
+import { Toolbar } from "primereact/toolbar";
+import { Dialog } from "primereact/dialog";
+import { classNames } from "primereact/utils";
+import { InputNumber } from "primereact/inputnumber";
 
 function App() {
+  let emptyProduct = {
+    hero_id: 0,
+    name: "",
+    eye_color: "",
+    hair_color: "",
+    skin_color: null,
+    height: 0,
+    weight: 0,
+    race: "",
+    publisher_id: 0,
+    gender_id: 0,
+    alignment_id: 0,
+  };
+
   const [products, setProducts] = useState([]);
+
+
+  const [product, setProduct] = useState(emptyProduct);
+
+
+  const [productDialog, setProductDialog] = useState(false);
+  const [deleteProductDialog, setDeleteProductDialog] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
+  const [globalFilter, setGlobalFilter] = useState(null);
+  const toast = useRef(null);
 
   useEffect(() => {
     heroService.getHerosData().then((data) => setProducts(data));
   }, []);
 
+  //Nuevo Producto
+  const openNew = () => {
+    setProduct(emptyProduct);
+    setSubmitted(false);
+    setProductDialog(true);
+  };
+  // Dialog -----
+  const hideDialog = () => {
+    setSubmitted(false);
+    setProductDialog(false);
+  };
+
+  const hideDeleteProductDialog = () => {
+    setDeleteProductDialog(false);
+  };
+
+  // Guardar Producto
+  const saveProduct = () => {
+    setSubmitted(true);
+
+    if (product.name.trim()) {
+      let _products = [...products];
+      let _product = { ...product };
+
+      if (product.id) {
+        const index = findIndexById(product.id);
+
+        _products[index] = _product;
+        toast.current.show({
+          severity: "success",
+          summary: "Successful",
+          detail: "Product Updated",
+          life: 3000,
+        });
+      } else {
+        _product.id = createId();
+        _product.image = "product-placeholder.svg";
+        _products.push(_product);
+        toast.current.show({
+          severity: "success",
+          summary: "Successful",
+          detail: "Product Created",
+          life: 3000,
+        });
+      }
+
+      setProducts(_products);
+      setProductDialog(false);
+      setProduct(emptyProduct);
+    }
+  };
+
+  // Opcion Lapiz de Editar
+  const editProduct = (product) => {
+    setProduct({ ...product });
+    setProductDialog(true);
+  };
+
+  // Opcion de Delete -----------------
+
+  
+  const confirmDeleteProduct = (product) => {
+    setProduct(product);
+    setDeleteProductDialog(true);
+  };
+
+  const deleteProduct = () => {
+    let _products = products.filter((val) => val.id !== product.id);
+
+    setProducts(_products);
+    setDeleteProductDialog(false);
+    setProduct(emptyProduct);
+    toast.current.show({
+      severity: "success",
+      summary: "Successful",
+      detail: "Product Deleted",
+      life: 3000,
+    });
+  };
+
+  // Find IndexBy
+  const findIndexById = (id) => {
+    let index = -1;
+
+    for (let i = 0; i < products.length; i++) {
+      if (products[i].id === id) {
+        index = i;
+        break;
+      }
+    }
+
+    return index;
+  };
+
+  // Crear Id de producto
+
+  const createId = () => {
+    let id = "";
+    /*let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (let i = 0; i < 5; i++) {
+      id += chars.charAt(Math.floor(Math.random() * chars.length));
+  }*/
+
+    return id;
+  };
+
+  /*
   const onRowEditComplete = (e) => {
     let _products = [...products];
     let { newData, index } = e;
 
     _products[index] = newData;
-    
+
     console.log(newData);
     heroService.editHerosData(newData).then((data) => setProducts(_products));
   };
@@ -33,38 +175,264 @@ function App() {
         onChange={(e) => options.editorCallback(e.target.value)}
       />
     );
+  };*/
+
+  //
+  const leftToolbarTemplate = () => {
+    return (
+      <div className="flex flex-wrap gap-2">
+        <Button
+          label="New"
+          icon="pi pi-plus"
+          severity="success"
+          onClick={openNew}
+        />
+      </div>
+    );
   };
 
-  const allowEdit = (rowData) => {
-    return true;
-};
+  // Body ------- botones de editar y eliminar
+
+  /*
+  const imageBodyTemplate = (rowData) => { //cambie rowData por data
+    return (
+      <img
+        src={`https://primefaces.org/cdn/primereact/images/product/${rowData.image}`}
+        alt={rowData.image}
+        className="shadow-2 border-round"
+        style={{ width: "64px" }}
+      />
+    );
+  };*/
+  const actionBodyTemplate = (rowData) => {
+    return (
+      <React.Fragment>
+        <Button
+          icon="pi pi-pencil"
+          rounded
+          outlined
+          className="mr-2"
+          onClick={() => editProduct(rowData)}
+        />
+        <Button
+          icon="pi pi-trash"
+          rounded
+          outlined
+          severity="danger"
+          onClick={() => confirmDeleteProduct(rowData)}
+        />
+      </React.Fragment>
+    );
+  };
+
+  // Heder busqueda
+  const header = (
+    <div className="flex flex-wrap gap-2 align-items-center justify-content-between">
+      <h4 className="m-0">Listado de Heroes</h4>
+      <IconField iconPosition="left">
+        <InputIcon className="pi pi-search" />
+        <InputText
+          type="search"
+          onInput={(e) => setGlobalFilter(e.target.value)}
+          placeholder="Search..."
+        />
+      </IconField>
+    </div>
+  );
+  // Dialog Eliminar
+  const productDialogFooter = (
+    <React.Fragment>
+      <Button label="Cancel" icon="pi pi-times" outlined onClick={hideDialog} />
+      <Button label="Save" icon="pi pi-check" onClick={saveProduct} />
+    </React.Fragment>
+  );
+
+  const deleteProductDialogFooter = (
+    <React.Fragment>
+      <Button
+        label="No"
+        icon="pi pi-times"
+        outlined
+        onClick={hideDeleteProductDialog}
+      />
+      <Button
+        label="Yes"
+        icon="pi pi-check"
+        severity="danger"
+        onClick={deleteProduct}
+      />
+    </React.Fragment>
+  );
+
+  const onInputChange = (e, name) => {
+    const val = (e.target && e.target.value) || "";
+    let _product = { ...product };
+
+    _product[`${name}`] = val;
+
+    setProduct(_product);
+  };
+
+
+  const onInputNumberChange = (e, name) => {
+    const val = e.value || 0;
+    let _product = { ...product };
+
+    _product[`${name}`] = val;
+
+    setProduct(_product);
+  };
+
   return (
     <div className="text-center">
+      <Toast ref={toast} />
       <div className="card">
+        <Toolbar className="mb-4" left={leftToolbarTemplate}></Toolbar>
+
         <DataTable
           value={products}
-          tableStyle={{ minWidth: "50rem" }}
-          editMode="row"
-          onRowEditComplete={onRowEditComplete}
+          //tableStyle={{ minWidth: "50rem" }}
+          //editMode="row"
+          //onRowEditComplete={}
+          paginator
+          rows={10}
+          rowsPerPageOptions={[5, 10, 25]}
+          paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
+          currentPageReportTemplate="Showing {first} to {last} of {totalRecords} products"
+          globalFilter={globalFilter}
+          header={header}
         >
           <Column
             field="name"
             header="Name"
-            editor={(options) => textEditor(options)}
+            //selectionMode="multiple"
+            //editor={(options) => textEditor(options)}
           ></Column>
+
           <Column field="publisher_id" header="Casa Publicadora"></Column>
           <Column field="gender_id" header="Género"></Column>
           <Column field="height" header="Altura"></Column>
           <Column field="weight" header="Peso"></Column>
+
           <Column
-            rowEditor={allowEdit}
-            headerStyle={{ width: "10%", minWidth: "8rem" }}
-            bodyStyle={{ textAlign: "center" }}
+            body={actionBodyTemplate}
+            exportable={false}
+            style={{ minWidth: "12rem" }}
           ></Column>
         </DataTable>
       </div>
+
+      <Dialog
+        visible={deleteProductDialog}
+        style={{ width: "32rem" }}
+        breakpoints={{ "960px": "75vw", "641px": "90vw" }}
+        header="Confirm"
+        modal
+        footer={deleteProductDialogFooter}
+        onHide={hideDeleteProductDialog}
+      >
+        <div className="confirmation-content">
+          <i
+            className="pi pi-exclamation-triangle mr-3"
+            style={{ fontSize: "2rem" }}
+          />
+          {product && (
+            <span>
+              Are you sure you want to delete <b>{product.name}</b>?
+            </span>
+          )}
+        </div>
+      </Dialog>
+
+      <Dialog
+        visible={productDialog}
+        style={{ width: "32rem" }}
+        breakpoints={{ "960px": "75vw", "641px": "90vw" }}
+        header="Product Details"
+        modal
+        className="p-fluid"
+        footer={productDialogFooter}
+        onHide={hideDialog}
+      >
+        {product.image && (
+          <img
+            src={`https://primefaces.org/cdn/primereact/images/product/${product.image}`}
+            alt={product.image}
+            className="product-image block m-auto pb-3"
+          />
+        )}
+        <div className="field">
+          <label htmlFor="name" className="font-bold">
+            Nombre
+          </label>
+          <InputText
+            id="name"
+            value={product.name}
+            onChange={(e) => onInputChange(e, "name")}
+            required
+            autoFocus
+            className={classNames({ "p-invalid": submitted && !product.name })}
+          />
+          {submitted && !product.name && (
+            <small className="p-error">Name is required.</small>
+          )}
+        </div>
+        <div className="field">
+          <label htmlFor="publisher_id" className="font-bold">
+            Casa Publicadora
+          </label>
+          <InputText
+            id="publisher_id"
+            value={product.publisher_id}
+            onChange={(e) => onInputChange(e, "publisher_id")}
+            required
+            autoFocus
+            className={classNames({
+              "p-invalid": submitted && !product.publisher_id,
+            })}
+          />
+        </div>
+
+        <div className="field">
+          <label htmlFor="gender_id" className="font-bold">
+            Género
+          </label>
+          <InputText
+            id="gender_id"
+            value={product.gender_id}
+            onChange={(e) => onInputChange(e, "gender_id")}
+            required
+            autoFocus
+            className={classNames({
+              "p-invalid": submitted && !product.gender_id,
+            })}
+          />
+        </div>
+
+        <div className="formgrid grid">
+          <div className="field col">
+            <label htmlFor="height" className="font-bold">
+              Altura
+            </label>
+            <InputNumber
+              id="height"
+              value={product.height}
+              onValueChange={(e) => onInputNumberChange(e, "height")}
+            />
+          </div>
+          <div className="field col">
+            <label htmlFor="weight" className="font-bold">
+              Peso
+            </label>
+            <InputNumber
+              id="weight"
+              value={product.weight}
+              onValueChange={(e) => onInputNumberChange(e, "weight")}
+            />
+          </div>
+        </div>
+      </Dialog>
     </div>
   );
 }
-
 export default App;
